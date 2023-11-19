9707be574da83c461b8095289b461ba9c2bdcfbe
samiunique <yitesam@gmail.com>
2023-11-18 21:14:56 -0800

    all

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *s_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void s_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * s_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void s_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
